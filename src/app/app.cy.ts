c9ceb4bedc44af91c7840ef6573a3c4cb26f5498
Filip Cieslik <filip.cieslik@vattenfall.com>
2023-07-31 22:44:06 +0200

    prettier fix

--- src/app/app.cy.ts
+++ src/app/app.cy.ts
@@ -3,7 +3,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   it('should show some gibberish stuff from ng template', () => {
     cy.mount(AppComponent);
-    cy.get('.new').contains('New Component')
-    cy.get('.material').contains('Angular Material')
+    cy.get('.new').contains('New Component');
+    cy.get('.material').contains('Angular Material');
   });
 });
